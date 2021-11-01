dadc23b7d69f97fb835da357e336d35b61ed42e2
koki <deguguti@yahoo.co.jp>
2021-11-01 17:55:47 +0900

    修正 intlを入れた(暫定。うまく動いていない)

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -17,12 +17,13 @@ class _MainPageState extends State<MainPage> {
 
   //日付を取得する
   getselectedDate(BuildContext context) async {
-    // initializeDateFormatting('ja');
+    Intl.defaultLocale = 'ja_JP';
+    await initializeDateFormatting('ja_JP');
     final selectedDate = await showDatePicker(
       initialDatePickerMode: DatePickerMode.year,
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime(1940),
+      firstDate: DateTime(1900),
       lastDate: DateTime.now(),
     );
     if (selectedDate != null) {
@@ -35,8 +36,6 @@ class _MainPageState extends State<MainPage> {
       );
     }
   }
-
-  //生まれてからの日付を取得する
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +59,7 @@ class _MainPageState extends State<MainPage> {
                 width: 300,
                 color: Colors.white70,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text(
                       'あなたの生まれた日は',
@@ -83,6 +83,7 @@ class _MainPageState extends State<MainPage> {
                 width: 300,
                 color: Colors.white70,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text(
                       'あなたの人生は',
